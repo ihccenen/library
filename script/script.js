33d2d278a494cf56a68399068465119a806b795a
ihccenen <wirkz01@gmail.com>
2022-10-03 16:28:56 -0300

    Added change status button

--- script/script.js
+++ script/script.js
@@ -11,6 +11,9 @@ Book.prototype.displayBook = function () {
   const tbody = document.querySelector(`[data-list='book-list']`);
   const tr = document.createElement('tr');
   const deleteBtn = document.createElement('button');
+
+  tr.dataset.book = tbody.childNodes.length;
+
   deleteBtn.type = 'button';
   deleteBtn.classList = 'delete-btn';
   deleteBtn.appendChild(document.createTextNode('X'));
@@ -22,12 +25,29 @@ Book.prototype.displayBook = function () {
     const td = document.createElement('td');
 
     td.appendChild(document.createTextNode(this[key]));
+
+    if (key === 'read') {
+      const changeStatusBtn = document.createElement('button');
+      changeStatusBtn.type = 'button';
+      changeStatusBtn.addEventListener('click', changeStatus);
+      td.appendChild(changeStatusBtn);
+    }
+
     tr.appendChild(td);
   }
 
   tr.appendChild(deleteBtn);
-  tr.dataset.book = tbody.childNodes.length;
   tbody.appendChild(tr);
+};
+
+Book.prototype.status = function () {
+  if (this.read === 'Finished') {
+    this.read = 'Not Yet';
+
+    return;
+  }
+
+  this.read = 'Finished';
 };
 
 function addBookToLibrary(event) {
@@ -56,6 +76,15 @@ function removeBook(event) {
 
     tr[index].dataset.book = index;
   });
+}
+
+function changeStatus(event) {
+  const td = event.target.parentElement;
+  const trNum = td.parentElement.dataset.book;
+
+  myLibrary[trNum].status();
+
+  td.firstChild.textContent = myLibrary[trNum].read;
 }
 
 const form = document.querySelector(`[data-form='book']`);
