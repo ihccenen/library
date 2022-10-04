a641d3a323d5b309e1917291cad7bf27e6365efe
ihccenen <wirkz01@gmail.com>
2022-10-04 19:08:51 -0300

    Added delete button

--- script/script.js
+++ script/script.js
@@ -29,7 +29,53 @@ function addBookToLibrary(event) {
   );
 
   myLibrary.push(newBook);
+  displayBook(newBook);
   clearForm();
+}
+
+function displayBook(book) {
+  const display = document.querySelector(`[data-display='container']`);
+  const div = document.createElement('div');
+  const deleteBtn = document.createElement('button');
+
+  div.classList.add('book-card', 'flex-container');
+  div.dataset.book = myLibrary.indexOf(book);
+  deleteBtn.type = 'button';
+  deleteBtn.classList.add('delete-btn');
+  deleteBtn.addEventListener('click', removeBook);
+
+  for (let key in book) {
+    if (!book.hasOwnProperty(key)) continue;
+
+    const textDiv = document.createElement('div');
+
+    textDiv.appendChild(
+      document.createTextNode(
+        `${key[0].toUpperCase() + key.slice(1)}: ${book[key]}`
+      )
+    );
+
+    div.appendChild(textDiv);
+  }
+
+  div.appendChild(deleteBtn);
+  display.appendChild(div);
+}
+
+function removeBook(event) {
+  const bookCard = event.target.parentElement;
+
+  myLibrary.splice(bookCard.dataset.book, 1);
+
+  bookCard.remove();
+
+  const display = Array.from(
+    document.querySelector(`[data-display='container']`).childNodes
+  );
+
+  display.map((book, index) => {
+    book.dataset.book = index;
+  });
 }
 
 function showForm() {
@@ -49,5 +95,6 @@ const form = document.querySelector(`[data-form='book']`);
 const btn = document.querySelector(`[data-button='new-book']`);
 const closeBtn = document.querySelector(`[data-button='close-modal']`);
 
+form.addEventListener('submit', addBookToLibrary);
 btn.addEventListener('click', showForm);
 closeBtn.addEventListener('click', clearForm);
