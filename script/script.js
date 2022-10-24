1072ddc33edb614b67e4cb068da25ff76398079b
ihccenen <110120407+ihccenen@users.noreply.github.com>
2022-10-24 17:55:09 -0300

    Changed from contructor to class

--- script/script.js
+++ script/script.js
@@ -1,119 +1,117 @@
-const myLibrary = [];
+const myLibrary = []
 
-function Book(...args) {
-  this.author = args[0];
-  this.title = args[1];
-  this.pages = args[2];
-  this.read = args[3];
-}
-
-Book.prototype.status = function () {
-  if (this.read === 'Finished') {
-    this.read = 'Not Yet';
-
-    return;
+class Book {
+  constructor(...args) {
+    ;[this.author, this.title, this.pages, this.read] = args
   }
 
-  this.read = 'Finished';
-};
+  status() {
+    if (this.read === 'Finished') {
+      this.read = 'Not Yet'
+      return
+    }
+
+    this.read = 'Finished'
+  }
+}
 
 function addBookToLibrary(event) {
-  event.preventDefault();
+  event.preventDefault()
 
-  const readStatus = form[4].checked ? 'Finished' : 'Not Yet';
+  const readStatus = form[4].checked ? 'Finished' : 'Not Yet'
   const newBook = new Book(
     form[0].value,
     form[1].value,
     form[2].value,
     readStatus
-  );
+  )
 
-  myLibrary.push(newBook);
-  displayBook(newBook);
-  clearForm();
+  myLibrary.push(newBook)
+  displayBook(newBook)
+  clearForm()
 }
 
 function displayBook(book) {
-  const display = document.querySelector(`[data-display]`);
-  const div = document.createElement('div');
-  const deleteBtn = document.createElement('button');
+  const display = document.querySelector(`[data-display]`)
+  const div = document.createElement('div')
+  const deleteBtn = document.createElement('button')
 
-  div.classList.add('book-card', 'flex-container');
-  div.dataset.book = myLibrary.indexOf(book);
-  deleteBtn.type = 'button';
-  deleteBtn.classList.add('delete-btn');
-  deleteBtn.addEventListener('click', removeBook);
+  div.classList.add('book-card', 'flex-container')
+  div.dataset.book = myLibrary.indexOf(book)
+  deleteBtn.type = 'button'
+  deleteBtn.classList.add('delete-btn')
+  deleteBtn.addEventListener('click', removeBook)
 
   for (let key in book) {
-    if (!book.hasOwnProperty(key)) continue;
+    if (!book.hasOwnProperty(key)) continue
 
-    const textDiv = document.createElement('div');
+    const textDiv = document.createElement('div')
 
     textDiv.appendChild(
       document.createTextNode(
         `${key[0].toUpperCase() + key.slice(1)}: ${book[key]}`
       )
-    );
+    )
 
     if (key === 'read') {
-      const statusBtn = document.createElement('button');
+      const statusBtn = document.createElement('button')
 
-      statusBtn.type = 'button';
-      statusBtn.classList.add('status-btn');
-      statusBtn.addEventListener('click', changeStatus);
-      textDiv.classList.add('status');
-      textDiv.appendChild(statusBtn);
+      statusBtn.type = 'button'
+      statusBtn.classList.add('status-btn')
+      statusBtn.addEventListener('click', changeStatus)
+      textDiv.classList.add('status')
+      textDiv.appendChild(statusBtn)
     }
 
-    div.appendChild(textDiv);
+    div.appendChild(textDiv)
   }
 
-  div.appendChild(deleteBtn);
-  display.appendChild(div);
+  div.appendChild(deleteBtn)
+  display.appendChild(div)
 }
 
 function removeBook(event) {
-  const bookCard = event.target.parentElement;
+  const bookCard = event.target.parentElement
 
-  myLibrary.splice(bookCard.dataset.book, 1);
+  myLibrary.splice(bookCard.dataset.book, 1)
 
-  bookCard.remove();
+  bookCard.remove()
 
   const display = Array.from(
     document.querySelector(`[data-display]`).childNodes
-  );
+  )
 
   display.map((book, index) => {
-    book.dataset.book = index;
-  });
+    book.dataset.book = index
+  })
 }
 
 function changeStatus(event) {
-  const text = event.target.parentElement;
-  const bookNum = text.parentElement.dataset.book;
+  const text = event.target.parentElement
+  const bookNum = text.parentElement.dataset.book
 
-  myLibrary[bookNum].status();
+  myLibrary[bookNum].status()
 
-  text.firstChild.textContent = `Read: ${myLibrary[bookNum].read}`;
+  text.firstChild.textContent = `Read: ${myLibrary[bookNum].read}`
 }
 
 function showForm() {
-  const modal = document.querySelector('.modal');
+  const modal = document.querySelector('.modal')
 
-  modal.style.display = 'flex';
+  modal.style.display = 'flex'
 }
 
 function clearForm() {
-  const modal = document.querySelector('.modal');
+  const modal = document.querySelector('.modal')
 
-  form.reset();
-  modal.style.display = 'none';
+  form.reset()
+  modal.style.display = 'none'
 }
 
-const form = document.querySelector(`[data-form]`);
-const btn = document.querySelector(`[data-button='new-book']`);
-const closeBtn = document.querySelector(`[data-button='close-modal']`);
+const form = document.querySelector(`[data-form]`)
+const btn = document.querySelector(`[data-button='new-book']`)
+const closeBtn = document.querySelector(`[data-button='close-modal']`)
 
-form.addEventListener('submit', addBookToLibrary);
-btn.addEventListener('click', showForm);
-closeBtn.addEventListener('click', clearForm);
+form.addEventListener('submit', addBookToLibrary)
+btn.addEventListener('click', showForm)
+closeBtn.addEventListener('click', clearForm)
